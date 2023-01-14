800b3240dff8cb4e255ea9bb2ccc4b0a1254ea44
technichron <sethpoulsen@gmail.com>
2023-01-14 10:47:50 -0500

    h

--- src/emulator.nim
+++ src/emulator.nim
@@ -1,3 +1,19 @@
 
 # APHELION EMULATOR 2.0
 # BY TECHNICHRON
+
+import std/strutils, std/sequtils
+
+var MemorySpace: array[0x10000, uint8]
+
+proc loadAMG(memarray: var array[0x10000, uint8], path: string) =
+    let amg = readFile(path)
+    if amg.len() == 0x10000:
+        for index in 0..0xFFFF:
+            memarray[index] = uint8(amg[index])
+    else:
+        echo "image file is improper length: 65536 bytes expected, got ", len(amg), " bytes"
+
+MemorySpace.loadAMG("h")
+
+echo MemorySpace[0]
